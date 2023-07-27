b191260e0dbf0956725f22a0357d7acb186f7a8b
Matt Johnson <mjohn23@gmail.com>
2023-07-27 16:24:29 -0500

    get job info including post date

---
+++ scraper.py
@@ -0,0 +1,31 @@
+import re, requests 
+from bs4 import BeautifulSoup
+from datetime import date
+
+url = 'https://www.unionjobs.com/staffing_list.php'
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content, 'html5lib')
+
+organizations = soup.find_all('div', {'class': 'organization'})
+
+for org in organizations:
+    organization_name = org.find_all('h3')[0].text
+    # remove multiple spaces from org names
+    # this converts something like UNITE HERE (Local                 1) to UNITE HERE (Local 1)
+    organization_name = re.sub(' +', ' ', organization_name)
+    jobs = org.find_all('li')
+    for job in jobs:
+        job_text = job.text.strip()
+
+        job_link = job.find('a')['href']
+        job_link = f'https://www.unionjobs.com{job_link}'
+        
+        job_title = job.find('a').text.strip()
+        
+        job_posted_string = re.search('\(Posted: (\d{1,20})\/(\d{1,2})\/(\d{4})\)', job_text)
+        job_posted_string_day = int(job_posted_string.group(1))
+        job_posted_string_month = int(job_posted_string.group(2))
+        job_posted_string_year = int(job_posted_string.group(3))
+        job_posted_date = date(job_posted_string_year, job_posted_string_day, job_posted_string_day)
+        print(job_posted_date)
